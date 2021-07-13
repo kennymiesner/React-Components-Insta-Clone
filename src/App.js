b12c2f1ee3e70dc774f023299be616648a502ca3
kennymiesner <kennymiesner@gmail.com>
2021-07-13 16:08:58 -0500

    Implemented posts and like button functionality

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ import './App.css';
 const App = () => {
   // Create a state called `posts` to hold the array of post objects, **initializing to dummyData**.
   // This state is the source of truth for the data inside the app. You won't be needing dummyData anymore.
-  // To make the search bar work (which is stretch) we'd need another state to hold the search term.
+  // To make the search bar work (which is stretch), we'd need another state to hold the search term.
   const [posts, setPosts] = useState(dummyData)
   const [searchTerm, setSearchTerm] = useState('')
 
@@ -31,14 +31,22 @@ const App = () => {
       The callback passed into `map` performs the following logic:
         - if the `id` of the post matches `postId`, return a new post object with the desired values (use the spread operator).
         - otherwise just return the post object unchanged.
-     */
+    */
+    const newPosts = posts.map(post => {
+      if (post.id === postId) {
+        return {...post, likes: post.likes + 1}
+      } else {
+        return post
+      }
+    })
+    setPosts(newPosts)
   };
 
   return (
     <div className='App'>
       {/* Add SearchBar and Posts here to render them */}
       <SearchBar />
-      <Posts />
+      <Posts posts={posts} likePost={likePost} />
       {/* Check the implementation of each component, to see what props they require, if any! */}
     </div>
   );
